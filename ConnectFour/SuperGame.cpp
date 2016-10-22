71a854113ca84f8d62ee87e6caebe4a49e022b46
HamishONE <hone075@aucklanduni.ac.nz>
2016-10-23 01:41:43 +1300

    Big SuperGame cleanup, add complexity spreadsheet

--- ConnectFour/SuperGame.cpp
+++ ConnectFour/SuperGame.cpp
@@ -1,14 +1,6 @@
 #include <set>
+#include <map>
 #include "SuperGame.hpp"
-
-SuperGame::SuperGame() {
-
-    // Call the parent class implementation first
-	Game();
-
-    // Initialise the winning player pointer to null
-	winningPlayer = 0;
-}
 
 bool SuperGame::playNextTurn(unsigned int column) {
 
@@ -33,34 +25,32 @@ bool SuperGame::playNextTurn(unsigned int column) {
     // Loop while there is still a chain of 4 discs
     while (findWinner() != 0) {
 
-        for (unsigned int i=0; i<findAllWinners().size(); i++) {
-            findAllWinners()[i]->increaseScore();
-        }
-        removeFour();
+        // Find all winners, removing the chains formed and incrementing their scores
+        findAllWinners();
     }
 
-    // Swap player
+    // Switch the current player
     if (currentPlayer == player1) {
         currentPlayer = player2;
     } else {
         currentPlayer = player1;
     }
 
+    // If any of the top cells are empty it is not a draw, so exit the function so the game can continue
     for (unsigned int i=0; i<grid->columnCount(); i++) {
         if (grid->cellAt(0, i) == Grid::GC_EMPTY) {
             return true;
         }
     }
 
+    // The game has finished so set the status to complete
     gameStatus = GS_COMPLETE;
 
+    // If one player has a higher score increment their win counter
     if (player1->getScore() > player2->getScore()) {
         player1->increaseWins();
-        winningPlayer = player1;
-    }
-    else if (player2->getScore() > player1->getScore()) {
+    } else if (player2->getScore() > player1->getScore()) {
         player2->increaseWins();
-        winningPlayer = player2;
     }
 
     return true;
@@ -73,38 +63,64 @@ const Player* SuperGame::winner() const {
         return 0;
     }
 
-    // Otherwise return the winner
-    return winningPlayer;
+    // If one player has a larger score they are the winner
+    if (player1->getScore() > player2->getScore()) {
+        return player1;
+    } else if (player2->getScore() > player1->getScore()) {
+        return player2;
+    }
+
+    // Otherwise return null
+    return 0;
 }
 
-void SuperGame::removeFour() {
+void SuperGame::findAllWinners() const {
 
     // Create a list of coordinates on the grid as a set to store discs to remove in a top-down order
     typedef std::pair<unsigned int, unsigned int> coord;
     std::set<coord> list;
 
-    // Loop through all spaces in the grid
+    // Create two booleans to store if the players should score a point
+    bool p1 = false, p2 = false;
+
+    // Create a mapping between the discs and these booleans
+    std::map<Grid::Cell, bool*> map;
+    map[Grid::GC_PLAYER_ONE] = &p1;
+    map[Grid::GC_PLAYER_TWO] = &p2;
+
+    // Loop through every cell on the board
     for (unsigned int i=0; i<grid->rowCount(); i++) {
         for (unsigned int j=0; j<grid->columnCount(); j++) {
-            Grid::Cell cell = grid->cellAt(i, j);
-            if (cell == Grid::GC_EMPTY) continue;
 
+            // If the cell is empty skip to the next cell
+            Grid::Cell cell = grid->cellAt(i, j);
+            if (cell == Grid::GC_EMPTY) {
+                continue;
+            }
+
+            // Check around each cell in the 4 directions for a line of 4 and if so:
+            //  - Set that players boolean to true
+            //  - Add all the 4 cells to the list to remove from
             if (grid->cellAt(i+1, j) == cell && grid->cellAt(i+2, j) == cell && grid->cellAt(i+3, j) == cell) {
+                *map[cell] = true;
                 for (unsigned int k=0; k<4; k++) {
                     list.insert(coord(i+k, j));
                 }
             }
             if (grid->cellAt(i, j+1) == cell && grid->cellAt(i, j+2) == cell && grid->cellAt(i, j+3) == cell) {
+                *map[cell] = true;
                 for (unsigned int k=0; k<4; k++) {
                     list.insert(coord(i, j+k));
                 }
             }
             if (grid->cellAt(i+1, j+1) == cell && grid->cellAt(i+2, j+2) == cell && grid->cellAt(i+3, j+3) == cell) {
+                *map[cell] = true;
                 for (unsigned int k=0; k<4; k++) {
                     list.insert(coord(i+k, j+k));
                 }
             }
             if (grid->cellAt(i+1, j-1) == cell && grid->cellAt(i+2, j-2) == cell && grid->cellAt(i+3, j-3) == cell) {
+                *map[cell] = true;
                 for (unsigned int k=0; k<4; k++) {
                     list.insert(coord(i+k, j-k));
                 }
@@ -112,44 +128,16 @@ void SuperGame::removeFour() {
         }
     }
 
+    // Remove all discs added to the list
     for (std::set<coord>::iterator it=list.begin(); it!=list.end(); it++) {
         grid->removeDisk(it->first, it->second);
     }
-}
 
-std::vector<Player*> SuperGame::findAllWinners() const {
-
-    bool p1 = false, p2 = false;
-
-    for (unsigned int i=0; i<grid->rowCount(); i++) {
-        for (unsigned int j=0; j<grid->columnCount(); j++) {
-
-            Grid::Cell cell = grid->cellAt(i, j);
-            if (cell == Grid::GC_EMPTY) {
-                continue;
-            }
-
-            if (grid->cellAt(i+1, j) == cell && grid->cellAt(i+2, j) == cell && grid->cellAt(i+3, j) == cell) {
-                if (cell == Grid::GC_PLAYER_ONE) p1 = true;
-                else p2 = true;
-            }
-            if (grid->cellAt(i, j+1) == cell && grid->cellAt(i, j+2) == cell && grid->cellAt(i, j+3) == cell) {
-                if (cell == Grid::GC_PLAYER_ONE) p1 = true;
-                else p2 = true;
-            }
-            if (grid->cellAt(i+1, j+1) == cell && grid->cellAt(i+2, j+2) == cell && grid->cellAt(i+3, j+3) == cell) {
-                if (cell == Grid::GC_PLAYER_ONE) p1 = true;
-                else p2 = true;
-            }
-            if (grid->cellAt(i+1, j-1) == cell && grid->cellAt(i+2, j-2) == cell && grid->cellAt(i+3, j-3) == cell) {
-                if (cell == Grid::GC_PLAYER_ONE) p1 = true;
-                else p2 = true;
-            }
-        }
+    // Increase the score of players as appropriate
+    if (p1) {
+        player1->increaseScore();
     }
-
-    std::vector<Player*> winners;
-    if (p1) winners.push_back(player1);
-    if (p2) winners.push_back(player2);
-    return winners;
+    if (p2) {
+        player2->increaseScore();
+    }
 }
--- ConnectFour/SuperGame.hpp
+++ ConnectFour/SuperGame.hpp
@@ -5,13 +5,10 @@
 
 class SuperGame : public Game {
 public:
-	SuperGame();
     bool playNextTurn(unsigned int column);
     const Player* winner() const;
 private:
-    void removeFour();
-    std::vector<Player*> findAllWinners() const;
-    const Player *winningPlayer;
+    void findAllWinners() const;
 };
 
 #endif /* end of include guard: SUPERGAME_HPP */
